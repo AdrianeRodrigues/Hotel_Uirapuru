2c1dadc7fb7a01c004b51fba910ceb22041f1ba7
root <root@notebook.(none)>
2013-06-12 11:06:04 -0300

    Verificador alterado.

--- Hotel_Uirapuru/src/br/com/poo/hotel/utilitarios/Verificador.java
+++ Hotel_Uirapuru/src/br/com/poo/hotel/utilitarios/Verificador.java
@@ -2,12 +2,16 @@ package br.com.poo.hotel.utilitarios;
 
 public class Verificador {
 
-	public static boolean isNumero(String str) {
-		return str.matches("[0-9]");
+	public static boolean isNumero(String numero) {
+		return numero.matches("[0-9]");
 	}
 	
-	public static boolean validarEmail(String str) {
-		return str.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+	public static boolean validarEmail(String email) {
+		return email.matches("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
 		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+	}
+	
+	public static boolean validarCPF(String cpf) {
+		return cpf.matches("[0-9]{2,3}?\.[0-9]{3}?\.[0-9]{3}?\-[0-9]{2}?");
 	}
 }
